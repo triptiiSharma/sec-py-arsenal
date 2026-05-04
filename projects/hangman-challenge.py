8f9b7dba08067112b30e5aa638168e8b1761509a
Tripti Sharma <triptii.work@gmail.com>
2026-05-04 13:31:45 +0530

    completed hangman game

--- projects/hangman-challenge.py
+++ projects/hangman-challenge.py
@@ -11,13 +11,35 @@ name = input("Enter your name: ")
 print(f"Hello {name}! welcome to this hangman game")
 
 wordList = ["meow", "sherni", "tripti"]
+# create an empty list 
+# for each item in the secret_word add a "_" that will be printed to the console 
+# example if the word is hacker - "_", "_", "_", "_", "_", "_"
 
 secret_word = random.choice(wordList)
-
-userChoice = input("guess a letter: ").lower()
+display_word = []
 
 for letter in secret_word:
-    if letter == userChoice:
-        print("right!")
-    else:
-        print("incorrect!")
+    display_word += "_"
+
+print(display_word)
+
+# loop through each of the letters in the chosen word 
+# if the letter is in the word replace the "_" with the letter 
+# it should look like this - "_", "_", "c", "_", "_", "r"
+
+game_over = False
+
+while not game_over:
+
+    userChoice = input("guess a letter: ").lower()
+
+    for position in range(len(secret_word)):
+        letter = secret_word[position]
+        if letter == userChoice:
+            display_word[position] = letter 
+
+    print(display_word)
+
+    if "_" not in display_word:
+        print("You win!")
+        game_over = True 
